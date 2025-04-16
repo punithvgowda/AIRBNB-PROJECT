332b6189b1e20cc73338f3d4b09732865e132a23
punithv <punithvgowda419@gmail.com>
2025-04-16 20:22:51 +0530

    models creation

--- models/listing.js
+++ models/listing.js
@@ -2,10 +2,19 @@ const mongoose=require("mongoose");
 const schema=mongoose.schema;
 
 const listingschema=new schema({
-    title:String,
-    description:String,
-    image:String,
+    title:{
+        type:String,
+        required: true,
+    },
+    description:{
+        type:String,
+        required: true,
+    },
+    image:{   type:String,
+        required: true,
+    },
     price:Number,
     location:String,
     country:string,
 })
+module.exports=listingschema
