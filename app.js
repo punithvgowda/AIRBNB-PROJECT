0866ee6071646d3ceb78251a148bb33cfd136228
punithv <punithvgowda419@gmail.com>
2025-04-17 11:22:11 +0530

    everthing upto date

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose")
 const Listing=require("./models/listing.js")
+const path=require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({ extended: true }));
+
+
  
 let MONGOOSE_URL="mongodb://127.0.0.1:27017/AIRBNB";
 
@@ -26,14 +32,25 @@ main(
 .catch((err)=>{
     console.log(err)
 })
- app.get("/testing",async (req,res)=>{
-    const samplelisting=new Listing({
-        title:"golden palms",
-        description:"one among the top hotels in bengaluru",
-        image:"H",
-        price:2000,
-        location:"dasanapura,bengaluru",
-        country:"bharath"
-    });
-     await samplelisting.save();
+ //app.get("/testing",async (req,res)=>{
+   // const samplelisting=new Listing({
+      //  title:"golden palms",
+      //  description:"one among the top hotels in bengaluru",
+       // image:"H",
+       // price:2000,
+       // location:"dasanapura,bengaluru",
+       // country:"bharath"
+   // });
+   //  await samplelisting.save();
+//})
+//index route
+app.get("/listings", async (req, res) => {
+    const allListing = await Listing.find({});
+    res.render("listing/index.ejs",{ allListing });
+ });
+ //show route
+app.get("/listings/:id",async(req,res )=>{
+    let {id}=req.params;
+    const listings=await Listing.findById(id);
+    res.render("listing/show.ejs",{listings})
 })
